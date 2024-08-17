ae133930a86758fcaefc50bf1408cd0504f3f1cc
maxim12313 <maximkim16@gmail.com>
2024-08-16 23:29:38 -0700

    keybindings change, autopairs, and auto format

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@ require("config.lazy")
 -- must have options
 vim.o.hlsearch = true
 vim.o.incsearch = true
-vim.o.clipboard = 'unnamed'
+vim.o.clipboard = "unnamed"
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smartindent = true
-vim.o.autoindent = true
+-- vim.o.autoindent = true
 vim.o.number = true
 vim.o.scrolloff = 8
-vim.o.guifont = 'Input Mono 13'
+vim.o.guifont = "Input Mono 13"
 vim.o.swapfile = false
 vim.o.cursorline = true
 vim.o.termguicolors = true
@@ -19,46 +19,41 @@ vim.o.signcolumn = "yes"
 -- windows
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.equalalways = true
 
 -- file navigation
-vim.keymap.set('i', '<C-c>', '<ESC>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>w', "<C-^>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-c>", "<ESC>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", "<C-^>", { noremap = true, silent = true })
 
 -- non overwrite registers
-vim.keymap.set('v', '<leader>p', '"_dP', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>c', '"_c', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>c', '"_c', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>d', '"_d', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>d', '"_d', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>p", '"_dP', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", '"_c', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>c", '"_c', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>d", '"_d', { noremap = true, silent = true })
 
---emacs bindings 
-vim.keymap.set('i', '<C-f>', '<Right>', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-b>', '<Left>', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-n>', '<Down>', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-p>', '<Up>', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-a>', '<ESC>I', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-e>', '<ESC>A', {noremap = true, silent = true})
-vim.keymap.set('i', '<C-k>', '<Right><ESC>C', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
-
---custom insert
-vim.keymap.set('i', '<C-d>', '<Right><ESC>ea', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-h>', '<ESC>bi', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-l>', '<Delete>', { noremap = true, silent = true })
+--emacs bindings
+vim.keymap.set("i", "<C-f>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-b>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-n>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-p>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-a>", "<ESC>I", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-e>", "<ESC>A", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-k>", "<Right><ESC>C", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-BS>", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-Right>", "<ESC>ea", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-Left>", "<S-Left>", { noremap = true, silent = true })
 
 -- vertical movement
-vim.keymap.set('n', 'K', '4k', { noremap = true, silent = true })
-vim.keymap.set('n', 'J', '4j', { noremap = true, silent = true })
-vim.keymap.set('v', 'K', '4k', { noremap = true, silent = true })
-vim.keymap.set('v', 'J', '4j', { noremap = true, silent = true })
+vim.keymap.set("n", "K", "4k", { noremap = true, silent = true })
+vim.keymap.set("n", "J", "4j", { noremap = true, silent = true })
+vim.keymap.set("v", "K", "4k", { noremap = true, silent = true })
+vim.keymap.set("v", "J", "4j", { noremap = true, silent = true })
 
 -- indent
-vim.keymap.set('n', "<Tab>", ">>", { noremap = true, silent = true })
-vim.keymap.set('n', "<S-Tab>", "<<", { noremap = true, silent = true })
-vim.keymap.set('v', "<Tab>", ">gv", { noremap = true, silent = true })
-vim.keymap.set('v', "<S-Tab>", "<gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 
 --color
-vim.cmd.colorscheme "tender"
-
+vim.cmd.colorscheme("tender")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,16 +31,13 @@ require("lazy").setup({
 				vim.g.sonokai_enable_italic = false
 			end,
 		},
-		{ "Mofiqul/dracula.nvim" },
 		{ "projekt0n/github-nvim-theme" },
 		{ "rose-pine/neovim" },
 		{ "folke/tokyonight.nvim" },
-		{ "ribru17/bamboo.nvim" },
 		{ "navarasu/onedark.nvim" },
 		{ "askfiy/visual_studio_code" },
-		{ "tanvirtin/monokai.nvim" },
 		{ "jacoborus/tender.vim" },
-
+		{ "tanvirtin/monokai.nvim" },
 
 		-- lsp manager
 		{ "williamboman/mason.nvim" },
@@ -53,13 +50,7 @@ require("lazy").setup({
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "hrsh7th/nvim-cmp" },
 		{ "L3MON4D3/LuaSnip" },
-		{
-		  "ray-x/lsp_signature.nvim",
-		  event = "VeryLazy",
-		  opts = {},
-		  config = function(_, opts) require'lsp_signature'.setup(opts) end
-		},
-
+		{ "ray-x/lsp_signature.nvim" },
 
 		-- formatter
 		{ "stevearc/conform.nvim", opts = {} },
@@ -75,7 +66,7 @@ require("lazy").setup({
 		{
 			"nvim-telescope/telescope.nvim",
 			tag = "0.1.8",
-			dependencies = { 
+			dependencies = {
 				"BurntSushi/ripgrep",
 				"nvim-lua/plenary.nvim",
 			},
@@ -85,25 +76,25 @@ require("lazy").setup({
 		{ "folke/trouble.nvim" },
 
 		-- file manager
-		{ 'stevearc/oil.nvim' },
+		{ "stevearc/oil.nvim" },
 
 		-- folder
-		{'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async'},
+		{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
 
 		-- todo comments
 		{ "folke/todo-comments.nvim" },
 
 		-- icons
-  		{ "nvim-tree/nvim-web-devicons" },
+		{ "nvim-tree/nvim-web-devicons" },
 
 		--scrolling
-		{ 'karb94/neoscroll.nvim' },
+		{ "karb94/neoscroll.nvim" },
 
 		--auto pairs
-		{ 
-			'windwp/nvim-autopairs',
+		{
+			"windwp/nvim-autopairs",
 			event = "InsertEnter",
-    		config = true
+			config = true,
 		},
 	},
 	install = { colorscheme = { "habamax" } },
@@ -111,52 +102,53 @@ require("lazy").setup({
 })
 
 ----------------------------------------------lsp setup--------------------------------------------------
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-    -- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts) 
-  end
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		-- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+		vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+		vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+		vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+	end,
 })
 
 vim.diagnostic.config({
-	-- virtual_text = false,
-	virtual_text = {
-		prefix = '▲',
-		severity = { 
-			min = vim.diagnostic.severity.ERROR,
-		}
-	},
+	virtual_text = false,
+	-- virtual_text = {
+	-- 	prefix = "▲",
+	-- 	severity = {
+	-- 		min = vim.diagnostic.severity.ERROR,
+	-- 	},
+	-- },
 	signs = {
-		severity = { 
-			min = vim.diagnostic.severity.ERROR,
-		}
+		severity = {
+			min = vim.diagnostic.severity.WARN,
+		},
 	},
-	virtual_lines = true,
+	virtual_lines = false,
+	-- underline = false,
 	underline = {
 		severity = {
 			min = vim.diagnostic.severity.WARN,
-		}
+		},
 	},
 	update_in_insert = false,
 	float = { border = "rounded" },
 })
 
-vim.fn.sign_define('DiagnosticSignError', { text = '●', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '●', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '●', texthl = 'DiagnosticSignHint' })
+vim.fn.sign_define("DiagnosticSignError", { text = "●", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "●", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "●", texthl = "DiagnosticSignHint" })
 
 ----------------------------------------------mason setup--------------------------------------------------
 require("mason").setup({})
@@ -186,7 +178,7 @@ cmp.setup({
 	},
 	window = {
 		completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	performance = {
 		debounce = 60,
@@ -203,13 +195,12 @@ cmp.setup({
 })
 
 require("lsp_signature").setup({
+	floating_window = true,
 	floating_window_above_cur_line = true,
-	max_height = 2,
+	max_height = 3,
 	floating_window_off_y = -3,
 	hint_enable = false,
-	severity_sort = true,
 })
-
 
 -------------------------------------------tree-sitter setup--------------------------------------------
 require("nvim-treesitter.configs").setup({
@@ -220,7 +211,6 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 	},
 })
-
 
 vim.keymap.set("n", "[c", function()
 	require("treesitter-context").go_to_context(vim.v.count1)
@@ -238,8 +228,12 @@ require("conform").setup({
 	default_format_opts = {
 		lsp_format = "fallback",
 	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
---
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
 vim.api.nvim_set_keymap("n", "==", "gqq", { noremap = true, silent = true })
@@ -258,7 +252,7 @@ require("Comment").setup({
 -------------------------------------------file manager setup--------------------------------------------
 local oil = require("oil")
 oil.setup({
-    show_hidden = true,
+	show_hidden = true,
 	skip_confirm_for_simple_edits = true,
 })
 
@@ -277,10 +271,9 @@ telescope.setup({
 			".git/",
 			"%.o",
 			"%.out",
-			"%.dSYM/"
+			"%.dSYM/",
 		},
 	},
-
 })
 
 local builtin = require("telescope.builtin")
@@ -288,7 +281,7 @@ local utils = require("telescope.utils")
 vim.keymap.set("n", "<leader>f", builtin.find_files)
 
 --run relative search from oil dir if in oil or telescope curr buffer if in normal file
-function relativeSearch() 
+function relativeSearch()
 	if vim.bo.filetype == "oil" then
 		builtin.find_files({ cwd = oil.get_current_dir() })
 	else
@@ -309,19 +302,18 @@ require("trouble").setup({
 		},
 	},
 })
-vim.keymap.set('n', '<leader>t', '<cmd>Trouble diagnostics toggle<CR>')
-
+vim.keymap.set("n", "<leader>t", "<cmd>Trouble diagnostics toggle<CR>")
 
 -------------------------------------------folder setup--------------------------------------------
-vim.keymap.set('n', '\\', 'za', { noremap = true, silent = true })
-vim.keymap.set('v', '\\', 'zf', { noremap = true, silent = true })
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+vim.keymap.set("n", "<leade>e", "za", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>e", "zf", { noremap = true, silent = true })
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.fillchars = [[eob: ,fold: ,foldopen:-,foldsep: ,foldclose:+]]
-vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldcolumn = "0" -- '0' is not bad
 
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
@@ -329,17 +321,17 @@ vim.o.foldenable = true
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
 }
 local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
 for _, ls in ipairs(language_servers) do
-    require('lspconfig')[ls].setup({
-        capabilities = capabilities
-        -- you can add other fields for setting up lsp server in this table
-    })
+	require("lspconfig")[ls].setup({
+		capabilities = capabilities,
+		-- you can add other fields for setting up lsp server in this table
+	})
 end
-require('ufo').setup({
+require("ufo").setup({
 	open_fold_hl_timeout = 0,
 })
 
@@ -349,21 +341,21 @@ local todo = require("todo-comments").setup({
 })
 
 vim.keymap.set("n", "]t", function()
-  todo.jump_next()
+	todo.jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-  todo.jump_prev()
+	todo.jump_prev()
 end, { desc = "Previous todo comment" })
 
 -------------------------------------------scrolling setup--------------------------------------------
 
-neoscroll = require('neoscroll')
+neoscroll = require("neoscroll")
 
 neoscroll.setup({
-  easing = 'linear',          
-  cursor_scrolls_alone = false,
-  mappings = {},
+	easing = "linear",
+	cursor_scrolls_alone = false,
+	mappings = {},
 })
 
 local keymap = {
@@ -371,7 +363,7 @@ local keymap = {
 		vim.cmd("normal!H")
 		neoscroll.ctrl_u({ duration = 100 })
 	end,
-	["<C-d>"] = function()
+	["<A-Right>"] = function()
 		vim.cmd("normal!L")
 		neoscroll.ctrl_d({ duration = 100 })
 	end,
@@ -382,35 +374,32 @@ local keymap = {
 		neoscroll.ctrl_f({ duration = 300 })
 	end,
 }
-local modes = { 'n', 'v', 'x' }
+local modes = { "n", "v", "x" }
 for key, func in pairs(keymap) do
-  vim.keymap.set(modes, key, func)
+	vim.keymap.set(modes, key, func)
 end
-
-
 
 -------------------------------------------autopairs setup--------------------------------------------
 local autopairs = require("nvim-autopairs")
 autopairs.remove_rule("{")
 
 -- better { rules
-vim.keymap.set('i', '{', '{}<left>', {noremap = true, silent = true})
-vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O', {noremap = true, silent = true})
-vim.keymap.set('i', '{;<CR>', '{<CR>};<ESC>O', {noremap = true, silent = true})
-vim.keymap.set('i', '{,<CR>', '{<CR>},<ESC>O', {noremap = true, silent = true})
-vim.keymap.set('i', '{ ', '{}<Left><Space><Left><Space>', {noremap = true, silent = true})
-
+vim.keymap.set("i", "{", "{}<left>", { noremap = true, silent = true })
+vim.keymap.set("i", "{<CR>", "{<CR>}<ESC>O", { noremap = true, silent = true })
+vim.keymap.set("i", "{;<CR>", "{<CR>};<ESC>O", { noremap = true, silent = true })
+vim.keymap.set("i", "{,<CR>", "{<CR>},<ESC>O", { noremap = true, silent = true })
+vim.keymap.set("i", "{ ", "{}<Left><Space><Left><Space>", { noremap = true, silent = true })
 
 -------------------------------------------theme setup--------------------------------------------
 require("tokyonight").setup({
 	style = "storm", -- "night" or "storm"
 	on_highlights = function(hl, c)
 		hl.DiagnosticUnderlineWarn.undercurl = nil
-        hl.DiagnosticUnderlineWarn.underline = true
+		hl.DiagnosticUnderlineWarn.underline = true
 		hl.DiagnosticUnderlineError.undercurl = nil
-        hl.DiagnosticUnderlineError.underline = true
+		hl.DiagnosticUnderlineError.underline = true
 		hl.DiagnosticUnderlineHint.undercurl = nil
-        hl.DiagnosticUnderlineHint.underline = true
+		hl.DiagnosticUnderlineHint.underline = true
 	end,
 })
 require("visual_studio_code").setup({
@@ -419,11 +408,50 @@ require("visual_studio_code").setup({
 
 require("onedark").setup({
 	highlights = {
-		['@operator'] = { fg = "#89BEFA" }
+		["@operator"] = { fg = "#89BEFA" },
 	},
 	diagnostics = {
 		undercurl = false,
-	}
+	},
 })
 
+--remove italics always
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		-- Remove italic from all highlight groups
+		for _, group in ipairs(vim.fn.getcompletion("", "highlight")) do
+			local highlight = vim.api.nvim_get_hl_by_name(group, true)
+			if highlight and highlight.italic then
+				highlight.italic = nil
+				vim.api.nvim_set_hl(0, group, highlight)
+			end
+		end
+	end,
+})
 
+-- remove undercurls
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		-- Function to get the existing highlight attributes
+		local function get_hl(name)
+			return vim.api.nvim_get_hl_by_name(name, true)
+		end
+
+		-- Update diagnostic highlight groups
+		for _, group in ipairs({
+			"DiagnosticUnderlineError",
+			"DiagnosticUnderlineWarn",
+			"DiagnosticUnderlineInfo",
+			"DiagnosticUnderlineHint",
+		}) do
+			local hl = get_hl(group)
+			vim.api.nvim_set_hl(0, group, {
+				undercurl = false,
+				underline = true,
+				sp = hl["special"], -- Preserve the original 'sp' color
+			})
+		end
+	end,
+})
